820585a0a5a4f28ab2dcfb9fdb9cb0d2448f05d5
Davi Araki <davi.araki@gmail.com>
2022-01-29 13:32:29 -0300

    Create structure

---
+++ src/pages/start/index.tsx
@@ -0,0 +1,9 @@
+import { Image, StartPageContainer } from './styles';
+
+export function StartPage() {
+  return (
+    <StartPageContainer>
+      <Image></Image>
+    </StartPageContainer>
+  );
+}
---
+++ src/pages/start/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StartPageContainer = styled.div``;
+
+export const Image = styled.img``;
